afda0b7720d26be46f797cdf984420ed89edf739
Hugh Francis <me@hughfrancis.me>
2014-10-16 12:10:58 -0400

    basic server mocks

--- frontend/tests/integration/sanity-test.js
+++ frontend/tests/integration/sanity-test.js
@@ -1,19 +1,12 @@
 import Ember from 'ember';
 import { test } from 'ember-qunit';
 import startApp from '../helpers/start-app';
-import Pretender from 'pretender';
 
-var App,
-    Server;
+var App;
 
 module('Sanity Check', {
   setup: function() {
     App = startApp();
-    Server = new Pretender(function() {
-      this.get('/api/ams/products', function(request) {
-        return [200, { 'Content-Type': 'application/json' }, '{ "products": "access_token" }'];
-      });
-    });
   },
   teardown: function() {
     Ember.run(App, App.destroy);
