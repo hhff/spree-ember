8a8d5fa33d908ac64b616c6879f0d3f18e7a0f4f
Hugh Francis <me@hughfrancis.me>
2015-02-26 20:36:03 -0500

    add docs site to repo

---
+++ docs/site/checkouts/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Spree Ember.Checkouts"
+    ],
+    "modules": [
+        "ember-cli-spree-core_mixins_checkouts"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-cli-spree-core/mixins/checkouts",
+            "name": "ember-cli-spree-core_mixins_checkouts",
+            "description": "Provides Current Order and Checkout Functionality to the Spree service."
+        }
+    ]
+} };
+});
