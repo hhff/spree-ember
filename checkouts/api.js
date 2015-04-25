0abfbf72c6d634adc41cd0f9cab811018200ffb7
Hugh Francis <me@hughfrancis.me>
2015-04-25 19:43:34 -0400

    Updates

--- checkouts/api.js
+++ checkouts/api.js
@@ -1,17 +1,10 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "Spree Ember.Checkouts"
+        "CheckoutsMixin",
+        "OrderSerializer"
     ],
-    "modules": [
-        "spree-ember-core_mixins_checkouts"
-    ],
-    "allModules": [
-        {
-            "displayName": "spree-ember-core/mixins/checkouts",
-            "name": "spree-ember-core_mixins_checkouts",
-            "description": "Provides Current Order and Checkout Functionality to the Spree service."
-        }
-    ]
+    "modules": [],
+    "allModules": []
 } };
 });
