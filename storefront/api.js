c8f503315b06f6cf5cdf761561c757c5ea9f260b
Hugh Francis <me@hughfrancis.me>
2015-05-07 12:15:32 -0400

    Updates

--- storefront/api.js
+++ storefront/api.js
@@ -1,6 +1,30 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [],
+    "classes": [
+        "Component.SpreeAddress",
+        "Component.SpreeBreadcrumbs",
+        "Component.SpreeCheckout",
+        "Component.SpreeDetails",
+        "Component.SpreeGrid",
+        "Component.SpreeImages",
+        "Component.SpreeInput",
+        "Component.SpreeLineitems",
+        "Component.SpreeNavigation",
+        "Component.SpreeOrder",
+        "Component.SpreePdp",
+        "Component.SpreeProperties",
+        "Component.SpreeSelect",
+        "Component.SpreeShipment",
+        "Component.SpreeSource",
+        "Component.SpreeTaxonomies",
+        "Component.SpreeTaxonomy",
+        "Route.Cart",
+        "Route.Checkout",
+        "Route.OrdersShow",
+        "Route.ProductsIndex",
+        "Route.ProductsShow",
+        "Route.TaxonsIndex"
+    ],
     "modules": [],
     "allModules": []
 } };
